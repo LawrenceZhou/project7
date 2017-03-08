4289e20cd24b42d47f510e9be59ac9b2dde0117d
LawrenceZhou <hmskyijun@gmail.com>
2017-03-07 17:55:33 -0800

    1.1

--- schema/user.js
+++ schema/user.js
@@ -15,7 +15,7 @@ var userSchema = new mongoose.Schema({
     description: String,  // A brief user description
     occupation: String,    // Occupation of the user.
     login_name: String,
-    password: String
+    password: String,
 });
 
 // the schema is useless so far
