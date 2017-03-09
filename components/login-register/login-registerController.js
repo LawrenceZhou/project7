35e3f6c11eb91f21075a05dbbc8020e240064a9e
LawrenceZhou <hmskyijun@gmail.com>
2017-03-08 22:59:56 -0800

    5.2

--- components/login-register/login-registerController.js
+++ components/login-register/login-registerController.js
@@ -1,12 +1,26 @@
 'use strict';
 
-cs142App.controller('LoginRegisterController', ['$scope', '$routeParams', '$resource',
-  function($scope, $routeParams, $resource) {
+cs142App.controller('LoginRegisterController', ['$scope', '$routeParams', '$resource', '$location','$rootScope', '$http',
+  function($scope, $routeParams, $resource, $location, $rootScope, $http) {
       $scope.login = {};
       $scope.login.loginName = "";
       $scope.login.password = "";
+      $scope.login.statusInfo = "";
 
       $scope.loginClick = function() {
+          var url = '/admin/login';
+          var modelObj = JSON.stringify({login_name: $scope.login.loginName, password: $scope.login.password});
+          $http.post(url, modelObj).then(function successfCallback(response){
+              if(reponse.status === 400) {
+                  $scope.login.statusInfo = response.data;
+              }else if(reponse.status === 200) {
+                  console.log("log in successful");
+              }             
+          }, function errorCallback(response){
+              $scope.login.statusInfo = "webServer or network down";
+          }); 
+            
+
 
       }
 
