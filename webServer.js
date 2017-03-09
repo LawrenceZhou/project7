ced8480f27cf92b8d3d6d1e602a6064ab31a5099
LawrenceZhou <hmskyijun@gmail.com>
2017-03-09 14:34:18 -0800

    5.4

--- webServer.js
+++ webServer.js
@@ -392,6 +392,7 @@ app.post('/user', function(request, response, callback) {
     //if (!request.session.login_name) {
     //    return response.status(401).send("not log in");
     //}else {
+        console.log('create');
         var loginname = request.params.login_name;
         var pwd = request.params.password;
         var firstname = request.params.first_name;
