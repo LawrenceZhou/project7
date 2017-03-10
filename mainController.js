25b73980c1d9487a6b552fa7ee7409c88bcb4e18
LawrenceZhou <hmskyijun@gmail.com>
2017-03-09 20:04:36 -0800

    5.5

--- mainController.js
+++ mainController.js
@@ -69,6 +69,49 @@ cs142App.controller('MainController', ['$scope', '$resource', '$location', '$roo
                 //$scope.login.statusInfo = response.data;
               
           });
-        } 
+        }
+
+
+        //Upload Photo
+        var selectedPhotoFile;   // Holds the last file selected by the user
+     
+        // Called on file selection - we simply save a reference to the file in selectedPhotoFile
+        $scope.inputFileNameChanged = function (element) {
+            selectedPhotoFile = element.files[0];
+        };
+
+        // Has the user selected a file?
+        $scope.inputFileNameSelected = function () {
+            return !!selectedPhotoFile;
+        };
+
+        // Upload the photo file selected by the user using a post request to the URL /photos/new
+        $scope.uploadPhoto = function () {
+            console.log($scope.checkboxModel);
+
+            if (!$scope.inputFileNameSelected()) {
+                console.error("uploadPhoto called will no selected file");
+                return;
+            }
+            console.log('fileSubmitted', selectedPhotoFile);
+
+            // Create a DOM form and add the file to it under the name uploadedphoto
+            var domForm = new FormData();
+            domForm.append('uploadedphoto', selectedPhotoFile);
+            // Using $http to POST the form
+            $http.post('/photos/new', domForm, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined},
+            }).success(function(newPhoto){
+                $rootScope.$broadcast('photoAdded');
+                                        // The photo was successfully uploaded. XXX - Do whatever you want on success.
+            }).error(function(err){
+                // Couldn't upload the photo. XXX  - Do whatever you want on failure.
+                console.error('ERROR uploading photo', err);
+                
+            });
+        };  
+
+  
         
     }]);
