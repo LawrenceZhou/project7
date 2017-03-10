b0c33971c04ae6d9534c41ac5072340cc54b5c18
LawrenceZhou <hmskyijun@gmail.com>
2017-03-09 19:33:42 -0800

    5.5

--- components/user-photos/user-photosController.js
+++ components/user-photos/user-photosController.js
@@ -14,11 +14,36 @@ cs142App.controller('UserPhotosController', ['$scope', '$routeParams', '$resourc
         });
 
     var user = $resource('http://localhost:3000/user/:id', {}, {});
-        var object = user.get({id: userId}, function() {
-            $scope.user = object;
-            var firstName = $scope.user.first_name;
-            var lastName = $scope.user.last_name;
-            $scope.main.toolBar = "Photos of " + firstName + " " + lastName;
-            $scope.user.name = firstName + " " + lastName;
-        });
+    var object = user.get({id: userId}, function() {
+        $scope.user = object;
+        var firstName = $scope.user.first_name;
+        var lastName = $scope.user.last_name;
+        $scope.main.toolBar = "Photos of " + firstName + " " + lastName;
+        $scope.user.name = firstName + " " + lastName;
+    });
+
+    $scope.newComment ={}; //input comment
+    $scope.postComment = function(photo_id) {
+      if($scope.newComment[photo_id]) {
+          var url = '/commentsOfPhoto/' + photo_id;
+          var modelObj = JSON.stringify({comment : $scope.newComment[photo_id]});
+
+          $http.post(url, modelObj).then(function successfCallback(response){
+              if(response.status === 200) {
+                  $rootScope.$broadcast('Commented');
+                  console.log("comment successful");
+                  var photo = $resource('http://localhost:3000/photosOfUser/'+userId, {}, {'query': {method: 'GET', isArray : true}});
+                  var photoList = photo.query({}, function() {
+                    $scope.photos = photoList;
+                  });
+              }             
+          }, function errorCallback(response){
+              if(response.status === 400) {
+                console.log("cooment unsuccessful");
+              }
+          }); 
+      }
+    }
+
+
   }]);
